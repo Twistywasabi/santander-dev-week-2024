f3b6c3eb042080bdebf2ac2fd380858334a2859f
TwistyWasabi17 <lucas-hideaki@hotmail.com>
2024-07-28 21:55:01 -0300

    Implementação da camada de negócio

---
+++ src/main/java/me/dio/santander_dev_week_2024/service/UserService.java
@@ -0,0 +1,13 @@
+package me.dio.santander_dev_week_2024.service;
+
+import me.dio.santander_dev_week_2024.domain.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+
+    User findById(Long id);
+
+    User create(User userToCreate);
+
+}
